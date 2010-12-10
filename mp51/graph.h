5496fcec54719b988615b115246129fa5e088019
Joseph Chysler <jdchrysl@svsu.edu>
2010-12-10 00:02:07 -0500

    Add skeleton to mp51

---
+++ mp51/graph.h
@@ -0,0 +1,9 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "node.h"
+
+class Graph {
+  
+};
---
+++ mp51/node.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+class Node {
+  
+};
